08a9c6b0b900d359ed5d95d3a52d70697df0e4de
Gerry Paul <gpaul12@nc.rr.com>
2023-03-26 15:17:02 -0400

    Create an array of questions for user input

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project? (Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter yout title!");
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
